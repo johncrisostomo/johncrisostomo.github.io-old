db93e6d1f10cbf701c7d614ae6b6345896c26557
John Crisostomo <johncrisostomo@protonmail.ch>
2017-07-14 06:46:59 +0800

    did some restructuring to make refactoring easier

---
+++ js/index.js
@@ -0,0 +1,45 @@
+$('document').ready(() => {
+  setInterval(function() {
+    $('#timedate').text(moment().format('HH:mm D-MMM-YY'));
+  }, 30 * 1000);
+
+  $('#timedate').text(moment().format('HH:mm D-MMM-YY'));
+
+  $('#window-size').text(
+    $(window).innerWidth() + 'x' + $(window).innerHeight()
+  );
+
+  $(window).resize(function() {
+    $('#window-size').text(
+      $(window).innerWidth() + 'x' + $(window).innerHeight()
+    );
+  });
+
+  $('.command').typewrite({
+    extra_char: '▓',
+    callback: function() {
+      setTimeout(function() {
+        const time = moment().format('MMM D H:mm');
+        const template =
+          '-rw-rw-r--&nbsp;&nbsp;&nbsp;1&nbsp;&nbsp;jdsc&nbsp;&nbsp;jdsc&nbsp;&nbsp;4096&nbsp;&nbsp;' +
+          time +
+          '&nbsp;&nbsp;&nbsp;&nbsp;';
+        $('.list').html(
+          "<ul><li><span class='temp'></span><a target='_blank' id='b' href='#''></a></li><li><span class='temp'></span><a target='_blank' id='g' href='#'></a></li><li><span class='temp'></span><a target='_blank' id='c' href='#'></a></li><li><span class='temp'></span><a target='_blank' id='fcc' href='#''></a></li></ul>"
+        );
+        $('.temp').html(template);
+        $('#b').text('blog');
+        $('#b').attr('href', 'https://blog.johncrisostomo.com');
+        $('#g').text('github');
+        $('#g').attr('href', 'https://github.com/johncrisostomo');
+        $('#c').text('codepen');
+        $('#c').attr('href', 'https://codepen.io/johncrisostomo/');
+        $('#fcc').text('freeCodeCamp');
+        $('#fcc').attr('href', 'https://www.freecodecamp.com/johncrisostomo');
+        $('.command').html('ls -l');
+        $('.cursor2').show();
+      }, 300);
+    },
+    delay: 200
+  });
+});
