18c2282b735e057702ce333c590a9a1a18752a30
John Crisostomo <johncrisostomo@protonmail.ch>
2017-10-16 17:18:20 +0800

    Add Instagram portfolio

--- js/index.js
+++ js/index.js
@@ -1,45 +1,53 @@
 $('document').ready(() => {
-  setInterval(function() {
+    setInterval(function() {
+        $('#timedate').text(moment().format('HH:mm D-MMM-YY'));
+    }, 30 * 1000);
+
     $('#timedate').text(moment().format('HH:mm D-MMM-YY'));
-  }, 30 * 1000);
 
-  $('#timedate').text(moment().format('HH:mm D-MMM-YY'));
-
-  $('#window-size').text(
-    $(window).innerWidth() + 'x' + $(window).innerHeight()
-  );
-
-  $(window).resize(function() {
     $('#window-size').text(
-      $(window).innerWidth() + 'x' + $(window).innerHeight()
+        $(window).innerWidth() + 'x' + $(window).innerHeight()
     );
-  });
 
-  $('.command').typewrite({
-    extra_char: '▓',
-    callback: function() {
-      setTimeout(function() {
-        const time = moment().format('MMM D H:mm');
-        const template =
-          '-rw-rw-r--&nbsp;&nbsp;&nbsp;1&nbsp;&nbsp;jdsc&nbsp;&nbsp;jdsc&nbsp;&nbsp;4096&nbsp;&nbsp;' +
-          time +
-          '&nbsp;&nbsp;&nbsp;&nbsp;';
-        $('.list').html(
-          "<ul><li><span class='temp'></span><a target='_blank' id='b' href='#''></a></li><li><span class='temp'></span><a target='_blank' id='g' href='#'></a></li><li><span class='temp'></span><a target='_blank' id='c' href='#'></a></li><li><span class='temp'></span><a target='_blank' id='fcc' href='#''></a></li></ul>"
+    $(window).resize(function() {
+        $('#window-size').text(
+            $(window).innerWidth() + 'x' + $(window).innerHeight()
         );
-        $('.temp').html(template);
-        $('#b').text('blog');
-        $('#b').attr('href', 'https://blog.johncrisostomo.com');
-        $('#g').text('github');
-        $('#g').attr('href', 'https://github.com/johncrisostomo');
-        $('#c').text('codepen');
-        $('#c').attr('href', 'https://codepen.io/johncrisostomo/');
-        $('#fcc').text('freeCodeCamp');
-        $('#fcc').attr('href', 'https://www.freecodecamp.com/johncrisostomo');
-        $('.command').html('ls -l');
-        $('.cursor2').show();
-      }, 300);
-    },
-    delay: 200
-  });
+    });
+
+    $('.command').typewrite({
+        extra_char: '▓',
+        callback: function() {
+            setTimeout(function() {
+                const time = moment().format('MMM D H:mm');
+                const template =
+                    '-rw-rw-r--&nbsp;&nbsp;&nbsp;1&nbsp;&nbsp;jdsc&nbsp;&nbsp;jdsc&nbsp;&nbsp;4096&nbsp;&nbsp;' +
+                    time +
+                    '&nbsp;&nbsp;&nbsp;&nbsp;';
+                $('.list').html(
+                    "<ul><li><span class='temp'></span><a target='_blank' id='b' href='#''></a></li><li><span class='temp'></span><a target='_blank' id='g' href='#'></a></li><li><span class='temp'></span><a target='_blank' id='c' href='#'></a></li><li><span class='temp'></span><a target='_blank' id='fcc' href='#''></a></li><li><span class='temp'></span><a target='_blank' id='instagram' href='#''></a></li></ul>"
+                );
+                $('.temp').html(template);
+                $('#b').text('blog');
+                $('#b').attr('href', 'https://blog.johncrisostomo.com');
+                $('#g').text('github');
+                $('#g').attr('href', 'https://github.com/johncrisostomo');
+                $('#c').text('codepen');
+                $('#c').attr('href', 'https://codepen.io/johncrisostomo/');
+                $('#fcc').text('freeCodeCamp');
+                $('#fcc').attr(
+                    'href',
+                    'https://www.freecodecamp.com/johncrisostomo'
+                );
+                $('#instagram').text('Instagram');
+                $('#instagram').attr(
+                    'href',
+                    'https://instagram.com/_hackneyed'
+                );
+                $('.command').html('ls -l');
+                $('.cursor2').show();
+            }, 300);
+        },
+        delay: 200
+    });
 });
